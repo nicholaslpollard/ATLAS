961d75da9f4af52950f0807a1101d99d71afd044
nicholaslpollard <nicholaslpollard@gmail.com>
2026-08-15 21:07:09 -0400

    Preserve data promotion errors during temp cleanup

--- packages/data/atomic.py
+++ packages/data/atomic.py
@@ -18,5 +18,8 @@ def promote(temp_path: Path, final_path: Path) -> None:
     try:
         replace_with_retry(temp_path, Path(final_path))
     except Exception:
-        temp_path.unlink(missing_ok=True)
+        try:
+            temp_path.unlink(missing_ok=True)
+        except OSError:
+            pass
         raise
