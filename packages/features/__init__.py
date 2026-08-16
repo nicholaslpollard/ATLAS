710c5a75e30090457dc41057f8c3e333f7d57d0e
nicholaslpollard <nicholaslpollard@gmail.com>
2026-08-16 14:32:01 -0400

    Phase 6C: expose feature persistence APIs

--- packages/features/__init__.py
+++ packages/features/__init__.py
@@ -5,24 +5,50 @@ from packages.features.feature_registry import (
     FeatureDefinition,
     FeatureRegistry,
 )
+from packages.features.historical_materializer import (
+    FeatureBootstrapRequired,
+    FeatureRangeMaterializationResult,
+    FeatureSessionMaterializationResult,
+    HistoricalFeatureMaterializer,
+)
 from packages.features.incremental import (
     IncrementalFeatureEngine,
     IncrementalFeatureError,
     IncrementalSymbolFeatureState,
 )
+from packages.features.materialization import (
+    ACTIVE_FEATURE_PERSISTENCE_POLICY,
+    FeaturePersistencePolicy,
+)
+from packages.features.partition_store import (
+    FeaturePartitionManifest,
+    FeaturePartitionStore,
+)
 from packages.features.session import regular_session_features
-from packages.features.state_checkpoint import FeatureStateCheckpointStore
+from packages.features.state_checkpoint import (
+    FeatureStateCheckpointStore,
+    feature_state_fingerprint,
+)
 
 __all__ = [
+    "ACTIVE_FEATURE_PERSISTENCE_POLICY",
     "CORE_FEATURE_CONTRACT_VERSION",
     "CORE_FEATURE_REGISTRY",
+    "FeatureBootstrapRequired",
     "FeatureDefinition",
     "FeatureInputError",
+    "FeaturePartitionManifest",
+    "FeaturePartitionStore",
+    "FeaturePersistencePolicy",
+    "FeatureRangeMaterializationResult",
     "FeatureRegistry",
+    "FeatureSessionMaterializationResult",
     "FeatureStateCheckpointStore",
+    "HistoricalFeatureMaterializer",
     "IncrementalFeatureEngine",
     "IncrementalFeatureError",
     "IncrementalSymbolFeatureState",
     "compute_core_features",
+    "feature_state_fingerprint",
     "regular_session_features",
 ]
