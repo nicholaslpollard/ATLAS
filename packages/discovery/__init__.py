85f69c70fb8683d618f215eaa31894503b4fbb4a
nicholaslpollard <nicholaslpollard@gmail.com>
2026-08-17 15:15:44 -0400

    Export discovery persistence contracts

--- packages/discovery/__init__.py
+++ packages/discovery/__init__.py
@@ -11,6 +11,14 @@ from .input_inventory import (
     DiscoveryInputInventory,
     DiscoveryInputInventoryReport,
 )
+from .persistence import (
+    ACTIVE_DISCOVERY_PERSISTENCE_POLICY,
+    DISCOVERY_PERSISTENCE_POLICY_VERSION,
+    DISCOVERY_STATE_MANIFEST_VERSION,
+    DiscoveryPersistencePolicy,
+    DiscoveryStateBuildResult,
+    DiscoveryStateManager,
+)
 from .scanner import (
     DISCOVERY_FOUNDATION_MANIFEST_VERSION,
     DiscoveryFoundationBuildResult,
@@ -24,16 +32,22 @@ from .scoring import (
 
 __all__ = [
     "ACTIVE_DISCOVERY_FILTER_POLICY",
+    "ACTIVE_DISCOVERY_PERSISTENCE_POLICY",
     "DISCOVERY_FILTER_POLICY_VERSION",
     "DISCOVERY_FOUNDATION_MANIFEST_VERSION",
     "DISCOVERY_INPUT_INVENTORY_CONTRACT_VERSION",
+    "DISCOVERY_PERSISTENCE_POLICY_VERSION",
     "DISCOVERY_SCORE_MANIFEST_VERSION",
+    "DISCOVERY_STATE_MANIFEST_VERSION",
     "DiscoveryFilterDecision",
     "DiscoveryFilterPolicy",
     "DiscoveryFoundationBuildResult",
     "DiscoveryFoundationScanner",
     "DiscoveryInputInventory",
     "DiscoveryInputInventoryReport",
+    "DiscoveryPersistencePolicy",
     "DiscoveryScoreBuildResult",
     "DiscoverySetupScanner",
+    "DiscoveryStateBuildResult",
+    "DiscoveryStateManager",
 ]
