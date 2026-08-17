1f5862ff12b132a1b3f8c0e5a737de4429a45b59
nicholaslpollard <nicholaslpollard@gmail.com>
2026-08-17 10:53:55 -0400

    Fix instrument package exports

--- packages/instruments/__init__.py
+++ packages/instruments/__init__.py
@@ -1,10 +1,8 @@
-from .continuity import InstrumentContinuityReconciler
 from .identity import IDENTITY_CONTRACT_VERSION, InstrumentIdentityResolver
 from .registry import InstrumentRegistryStore
 
 __all__ = [
     "IDENTITY_CONTRACT_VERSION",
-    "InstrumentContinuityReconciler",
     "InstrumentIdentityResolver",
     "InstrumentRegistryStore",
 ]
