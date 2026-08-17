659dfae2cb867042cf944a1bdee7fbe232176579
nicholaslpollard <nicholaslpollard@gmail.com>
2026-08-17 14:04:36 -0400

    Initialize Phase 8 discovery package

---
+++ packages/discovery/__init__.py
@@ -0,0 +1,13 @@
+"""ATLAS broad-market discovery foundation."""
+
+from .input_inventory import (
+    DISCOVERY_INPUT_INVENTORY_CONTRACT_VERSION,
+    DiscoveryInputInventory,
+    DiscoveryInputInventoryReport,
+)
+
+__all__ = [
+    "DISCOVERY_INPUT_INVENTORY_CONTRACT_VERSION",
+    "DiscoveryInputInventory",
+    "DiscoveryInputInventoryReport",
+]
