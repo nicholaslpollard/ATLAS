16295f8ab39ec1af5aff0b79736e2a9ee8025c4b
nicholaslpollard <nicholaslpollard@gmail.com>
2026-08-17 13:03:56 -0400

    Export universe policy and builder

--- packages/universe/__init__.py
+++ packages/universe/__init__.py
@@ -1,10 +1,21 @@
 from packages.schemas.universe import (
     UNIVERSE_CONTRACT_VERSION,
+    UniverseExclusion,
     UniverseMember,
     UniverseReasonCode,
     UniverseRoute,
     UniverseSnapshot,
     universe_members_fingerprint,
+)
+from packages.universe.eligibility import (
+    ACTIVE_UNIVERSE_ELIGIBILITY_POLICY,
+    UNIVERSE_ELIGIBILITY_POLICY_VERSION,
+    UniverseEligibilityPolicy,
+)
+from packages.universe.manager import (
+    UNIVERSE_MANIFEST_VERSION,
+    UniverseBuildResult,
+    UniverseManager,
 )
 from packages.universe.metadata import (
     REFERENCE_UNIVERSE_INVENTORY_VERSION,
@@ -12,8 +23,15 @@ from packages.universe.metadata import (
 )
 
 __all__ = [
+    "ACTIVE_UNIVERSE_ELIGIBILITY_POLICY",
     "REFERENCE_UNIVERSE_INVENTORY_VERSION",
     "UNIVERSE_CONTRACT_VERSION",
+    "UNIVERSE_ELIGIBILITY_POLICY_VERSION",
+    "UNIVERSE_MANIFEST_VERSION",
+    "UniverseBuildResult",
+    "UniverseEligibilityPolicy",
+    "UniverseExclusion",
+    "UniverseManager",
     "UniverseMember",
     "UniverseReasonCode",
     "UniverseReferenceInventory",
