2bab488c375bebc05478d48b28ccca73e06cc862
nicholaslpollard <nicholaslpollard@gmail.com>
2026-08-17 10:51:06 -0400

    Export corrected identity contract

--- packages/instruments/__init__.py
+++ packages/instruments/__init__.py
@@ -1,6 +1,10 @@
-"""Instrument identity and expression-selection components."""
-
-from .identity import InstrumentIdentityResolver
+from .continuity import InstrumentContinuityReconciler
+from .identity import IDENTITY_CONTRACT_VERSION, InstrumentIdentityResolver
 from .registry import InstrumentRegistryStore
 
-__all__ = ["InstrumentIdentityResolver", "InstrumentRegistryStore"]
+__all__ = [
+    "IDENTITY_CONTRACT_VERSION",
+    "InstrumentContinuityReconciler",
+    "InstrumentIdentityResolver",
+    "InstrumentRegistryStore",
+]
