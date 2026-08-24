68c6baa5e9f59c8482aeb02239f82fb0c0ebafc6
nicholaslpollard <nicholaslpollard@gmail.com>
2026-08-24 09:40:41 -0400

    Route Webull adapter through hardened package boundary

--- packages/brokers/webull/__init__.py
+++ packages/brokers/webull/__init__.py
@@ -1,3 +1,3 @@
-from .broker import WebullSandboxBroker
+from .safe_broker import WebullSandboxBroker, harden_webull_sdk_logging
 
-__all__ = ["WebullSandboxBroker"]
+__all__ = ["WebullSandboxBroker", "harden_webull_sdk_logging"]
